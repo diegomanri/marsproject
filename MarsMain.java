8048f06d94562fd41856a7617e797ce3f0266a32
Diego <diegomanri@gmail.com>
2014-06-10 22:38:02 -0500

    Deleted a comment in the main java class. Also testing this multiline comment thing.

--- MarsMain.java
+++ MarsMain.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class MarsMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
