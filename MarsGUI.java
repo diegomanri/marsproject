b41e3c2d0e64e34baa5d50fc1741c93591510b19
Diego <diegomanri@gmail.com>
2014-06-10 22:24:04 -0500

    Added MarsGUI.java file that will be used to create the GUI of the application

---
+++ MarsGUI.java
@@ -0,0 +1,19 @@
+package marsPackage;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MarsGUI extends JFrame {
+	
+	
+/*
+ * Constructor
+ */
+	
+	public MarsGUI(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("MARS 1.0");
+		setSize(1000, 900);
+	}
+
+}
