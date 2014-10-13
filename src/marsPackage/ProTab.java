012189c5b8edceb45a1610f79d4d94f39f745dc4
Diego <diegomanri@gmail.com>
2014-10-12 20:42:06 -0500

    Adding the Quick Search GUI section in the Project Tab. The quick search input field uses an event that deletes the default content as soon as the input field is clicked.

--- src/marsPackage/ProTab.java
+++ src/marsPackage/ProTab.java
@@ -1,14 +1,19 @@
 package marsPackage;
 
 import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.*;
 
 public class ProTab {
-    private JLabel testLabel;
+	private JPanel eastPanel;
+    private JButton advSbutton;
+    private JButton quickSbutton;
+    private JPanel westPanel;
     private JLabel testLabel2;
-    private JLabel testLabel3;
-    private JLabel testLabel4;
+    private JTextField quickSfield;
+    private JLabel quickSspace;
+    private JPanel quickSpanel;
     private JPanel proPanel;
     private JPanel contentPane;
     private JScrollPane scrollPane;
@@ -20,12 +25,12 @@ public class ProTab {
      */
     
     public ProTab(){
-    //Creating the proanel that holds everything
+    //Creating the propanel that holds everything
     proPanel = new JPanel();
     proPanel.setBackground(new Color(204, 255, 102));
     
     //Creating the contentPane that holds all GUI components and
-    //uses vertical/horizontal sidebards as needed
+    //uses vertical/horizontal sidebars as needed
     contentPane = new JPanel();
     contentPane.setBackground(Color.WHITE);
     
@@ -47,37 +52,60 @@ public class ProTab {
      * g variable.
      */
             
-    testLabel = new JLabel("Testing Here 1! :D");
-    testLabel.setPreferredSize(new Dimension(500, 500));
-    testLabel.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.gray));
+    
+    //QuickSearch GUI top section START
+     
+    quickSpanel = new JPanel();
+    quickSpanel.setBackground(Color.WHITE);
+    quickSpanel.setBorder(BorderFactory.createMatteBorder(0,0,2,0, Color.gray));
+    quickSpanel.setLayout(new BorderLayout());
     g.gridx = 0;
     g.gridy = 0;
-    contentPane.add(testLabel, g);
+    contentPane.add(quickSpanel, g);
+    
+    	eastPanel = new JPanel();
+    	eastPanel.setBackground(Color.WHITE);
+    	eastPanel.setLayout(new FlowLayout());
+    	quickSpanel.add(eastPanel, BorderLayout.EAST);
+    
+    		advSbutton = new JButton("Advanced Search");
+    		eastPanel.add(advSbutton);
+    
+    	westPanel = new JPanel();
+    	westPanel.setBackground(Color.WHITE);
+    	westPanel.setLayout(new FlowLayout());
+    	quickSpanel.add(westPanel, BorderLayout.WEST);
+    
+    		quickSfield = new JTextField("QuickSearch - Enter project code", 17);
+    		westPanel.add(quickSfield);
+    		
+    		quickSfield.addMouseListener(new MouseAdapter(){
+    			public void mouseClicked(MouseEvent e){
+    				quickSfield.setText("");
+    			}
+    		});
+    
+    		quickSbutton = new JButton("Search");
+    		westPanel.add(quickSbutton);
+ 
+    	quickSspace = new JLabel();
+    	quickSspace.setPreferredSize(new Dimension(400, 0));
+    	quickSpanel.add(quickSspace, BorderLayout.CENTER);
+    
+    
+    //QuickSearch GUI top section END
+    
     
     testLabel2 = new JLabel("Testing Here 2");
     testLabel2.setPreferredSize(new Dimension(250, 200));
     testLabel2.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.gray));
-    g.gridx = 1;
-    g.gridy = 0;
-    contentPane.add(testLabel2, g);
-            
-    testLabel3 = new JLabel("Testing Here 3");
-    testLabel3.setPreferredSize(new Dimension(200, 200)); 
-    testLabel3.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.gray));
-    g.gridx = 1;
+    g.gridx = 0;
     g.gridy = 1;
-    contentPane.add(testLabel3, g);
-    
-    testLabel4 = new JLabel("Testing Here 4");
-    testLabel4.setPreferredSize(new Dimension(200, 200)); 
-    testLabel4.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.gray));
-    g.gridx = 2;
-    g.gridy = 2;
-    contentPane.add(testLabel4, g);
+    contentPane.add(testLabel2, g);
     }
     
     /**
-     * The getEmpTab method returns a ProTab object.
+     * The getProTab method returns a ProTab object.
      * @return a ProTab panel object.
      */
 	public JPanel getProTab(){
